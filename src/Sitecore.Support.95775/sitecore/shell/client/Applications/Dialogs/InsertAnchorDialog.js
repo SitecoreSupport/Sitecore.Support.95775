e629130c3b3fed2f1a30bd7bba2b4846f500f732
DK\ili <ili@sitecore.net>
2017-05-11 17:44:03 +0300

    no message

---
+++ src/Sitecore.Support.95775/sitecore/shell/client/Applications/Dialogs/InsertAnchorDialog.js
@@ -0,0 +1,30 @@
+﻿define(["sitecore"], function(Sitecore) {
+    var InsertAnchorDialog = Sitecore.Definitions.App.extend({
+        initialized: function () {
+
+            this.updateOkButton();
+
+            this.Text.on("change", function () {
+                this.updateOkButton();
+            }, this);
+
+        },
+
+        updateOkButton: function () {
+
+            var text = this.Text.get("text");
+
+            if (text)
+                this.InsetAnchor.set("isEnabled", true);
+            else
+                this.InsetAnchor.set("isEnabled", false);
+        }
+
+    });
+    var footer = $("footer");
+    console.log("Break footer");
+    if (footer && footer.length > 0) {
+        footer.css("z-index", "4");
+    }
+    return InsertAnchorDialog;
+});
