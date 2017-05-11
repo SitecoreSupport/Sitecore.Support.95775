3915ba078d4c5145b18a2185c6ecb91f0fbab2d7
DK\ili <ili@sitecore.net>
2017-05-11 17:04:31 +0300

    Release

---
+++ src/Sitecore.Support.95775/sitecore/shell/client/Applications/Dialogs/InsertEmailDialog.js
@@ -0,0 +1,19 @@
+﻿define(["sitecore"], function (Sitecore) {
+  var myapp = Sitecore.Definitions.App.extend({
+      initialized: function() {
+          var text = this.DisplayedText.get("text");
+
+          var flag = (text != null && text != "");
+          if (flag) {
+              this.InsertEmailLink.set("isEnabled", "true");
+          }
+
+          var $el = $("footer");
+          if ($el && $el.length > 0) {
+              $el.css("z-index", "4");
+          }
+      }
+  });
+
+  return myapp;
+});
